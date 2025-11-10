3d4352116a63d078aa15860562126e0d7f55890c
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-10 21:46:28 +0600

    responsive the logo

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,10 +1,10 @@
 import { GiBookAura } from "react-icons/gi";
 export default function Logo() {
   return (
-    <div className='min-w-24 max-w-32'>
+    <div className='w-20 sm:w-22'>
       <div className='flex flex-col justify-center items-center border border-secondary-content p-1 rounded-md'>
-        <GiBookAura className='text-5xl' />
-        <p className='text-sm'>STUDY MATE</p>
+        <GiBookAura className='text-2xl sm:text-4xl' />
+        <p className='text-[10px] sm::text-xs font-bold'>STUDY MATE</p>
       </div>
     </div>
   );
