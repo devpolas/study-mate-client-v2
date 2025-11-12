0f06b125c004bffc99b3a43dae11acd300c6586c
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-12 20:31:35 +0600

    fix rootlayout design

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
   return (
     <div className='px-4 sm:px-6 md:px-16'>
       <Navbar />
-      <div className='min-h-[75vh] pb-4'>
+      <div className='min-h-[75vh] pb-12'>
         {isPending ? (
           <div className='skeleton h-[75vh] w-full'></div>
         ) : (
