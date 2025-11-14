52b76884a1c2eddececed2fc6ed40e39537cbe42
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-14 19:19:22 +0600

    show all mates data with filter and sort

--- src/pages/FindPartnerPage.jsx
+++ src/pages/FindPartnerPage.jsx
@@ -1,11 +1,68 @@
 import { useLoaderData } from "react-router";
 import MateIntro from "./../components/MateIntro";
+import { useEffect, useState } from "react";
 
 export default function FindPartnerPage() {
+  const [mate, setMate] = useState([]);
+  const [sortBy, setSortBy] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+
   const allUsers = useLoaderData();
-  const data = allUsers?.data?.users;
+  const usersData = allUsers?.data?.users || [];
+
+  // api search end point
+  //  *https://study-mate-api.vercel.app/api/v1/users?slug=query
+  //  *https://study-mate-api.vercel.app/api/v1/users?slug=query&sort=-name
+  // filter and sort use userLoaderData for better user experience
+
+  // FILTER (Search)
+  useEffect(() => {
+    setLoading(true);
+
+    const delay = setTimeout(() => {
+      if (!query) {
+        setMate(usersData);
+      } else {
+        const result = usersData.filter((item) =>
+          item.name.toLowerCase().includes(query.toLowerCase())
+        );
+        setMate(result);
+      }
+      setLoading(false);
+    }, 300);
+
+    return () => clearTimeout(delay);
+  }, [query, usersData]);
+
+  // SORT
+  useEffect(() => {
+    if (!sortBy) return;
+
+    setLoading(true);
+
+    const delay = setTimeout(() => {
+      const sorted = [...mate];
+
+      if (sortBy === "name") {
+        sorted.sort((a, b) => a.name.localeCompare(b.name));
+      } else if (sortBy === "rating") {
+        sorted.sort((a, b) => b.ratingAverage - a.ratingAverage);
+      } else if (sortBy === "experience") {
+        sorted.sort((a, b) =>
+          a.experienceLevel.localeCompare(b.experienceLevel)
+        );
+      }
+
+      setMate(sorted);
+      setLoading(false);
+    }, 300);
+
+    return () => clearTimeout(delay);
+  }, [sortBy]);
+
   return (
-    <div className=' pt-8'>
+    <div className='pt-8'>
       <div className='flex justify-center items-center sm:justify-around flex-col sm:flex-row'>
         <label className='input'>
           <svg
@@ -24,37 +81,42 @@ export default function FindPartnerPage() {
               <path d='m21 21-4.3-4.3'></path>
             </g>
           </svg>
-          <input type='search' required placeholder='Search' />
+          <input
+            type='search'
+            required
+            placeholder='Search'
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </label>
 
-        <form className='flex flex-row gap-1 ml-2 sm:ml-0 mt-4 sm:mt-0'>
-          <input
-            className='btn'
-            type='checkbox'
-            name='frameworks'
-            aria-label='Svelte'
-          />
-          <input
-            className='btn'
-            type='checkbox'
-            name='frameworks'
-            aria-label='Vue'
-          />
-          <input
-            className='btn'
-            type='checkbox'
-            name='frameworks'
-            aria-label='React'
-          />
-          <input className='btn btn-square' type='reset' value='×' />
-        </form>
+        <fieldset className='fieldset min-w-3xs'>
+          <select
+            onChange={(e) => setSortBy(e.target.value.toLowerCase())}
+            className='select'
+          >
+            <option disabled={true}>Sort By</option>
+            <option value='name'>Name</option>
+            <option value='rating'>Rating</option>
+            <option value='experience'>Experience Level</option>
+          </select>
+        </fieldset>
       </div>
+
       <div className='divider'></div>
-      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2'>
-        {data.map((mate) => (
-          <MateIntro {...mate} />
-        ))}
-      </div>
+
+      {loading ? (
+        <div className='flex  h-[30vh] max-w-full justify-center items-center'>
+          <span className='loading loading-spinner text-primary'></span>
+        </div>
+      ) : (
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2'>
+          {mate.length > 0 ? (
+            mate.map((mate) => <MateIntro key={mate._id} {...mate} />)
+          ) : (
+            <p className='text-2xl text-primary text-center'>No Mate Found!</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
