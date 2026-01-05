ba7cb198edca8ad45de853e13176c89189ccf5f2
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-01-05 23:38:20 +0600

    clean up the project

--- src/http/axiosPublic.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// https://study-mate-api.vercel.app/api/v1
-
-const axiosPublic = axios.create({
-  baseURL: "https://study-mate-api.vercel.app/api/v1",
-});
-
-export default axiosPublic;
